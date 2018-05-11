cc324755ba50df6ee2915c7b9da9f7ab08a93227
Armando Jr <elfiservice@hotmail.com>
2018-05-11 06:15:04 -0300

    feat: provided MapContainer compt

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1> Google Maps API + React </h1>
+        <h1> Here my Nav component </h1>
 
         <MapContainer google={this.props.google} />
       </div>
---
+++ src/Map.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Map extends Component {
+    render() {
+        return (
+            <div className="map-container">
+                here my Marker Component
+            </div>
+        )
+    }
+}
+
+export default Map;
